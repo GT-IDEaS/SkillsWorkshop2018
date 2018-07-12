6b3ec4decde3d217f906cb5a2732600894ac6cfa
Yifei Wang <yifei.wang.ac@gmail.com>
2018-07-12 17:46:54 -0400

    change style

--- Week01/Problem01/YWang_01.py
+++ Week01/Problem01/YWang_01.py
@@ -6,9 +6,11 @@ lim_UB = 1000
 
 # initialize save space
 multi_List = list()
+
 # find all the multiples of 3 or 5 below lim_UB
 for i in range(1,lim_UB):
     if i%3==0 or i%5==0:
         multi_List.append(i)
+        
 # print results
 print(sum(multi_List))
